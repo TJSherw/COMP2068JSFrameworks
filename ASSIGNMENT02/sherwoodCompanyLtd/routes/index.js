cd4dbf4482db7d7a7ae4fda71e7219aa6bf054f8
TJSherw <sherwood0419@hotmail.com>
2023-11-27 16:34:17 -0500

    Basic Home Page Design
    Basic Design Completed.
    Added 3 more pages, Pre-Workout, Protein, and Creatine Page. Also, added routes.

--- ASSIGNMENT02/sherwoodCompanyLtd/routes/index.js
+++ ASSIGNMENT02/sherwoodCompanyLtd/routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sherwood Company Limited' });
 });
+
+
+/* GET PreWorkout page. */
+router.get('/preworkout', function(req, res, next) {
+  res.render('preworkout', { title: 'Pre-Workout' });
+});
+
+
+
+/* Get Protein Page */ 
+router.get('/protein', function(req, res, next) {
+  res.render('protein', { title: 'Protein' });
+});
+
+/* GET Creatine page. */
+router.get('/creatine', function(req, res, next) {
+  res.render('creatine', { title: 'Creatine' });
+});
+
 
 module.exports = router;
